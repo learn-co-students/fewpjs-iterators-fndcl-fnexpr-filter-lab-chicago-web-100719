4c3a8bc565bee6ed329aef3df356efa22161d3e6
Valeria <valeria.gutsuliak@gmail.com>
2019-11-25 16:32:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(source, passed) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === passed.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, passedName) {
+    return source.filter( record => record.name === passedName)
+  }
